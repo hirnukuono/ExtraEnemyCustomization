535f0e5a0a9243870558ac6c60b2df299c2da856
Flowaria <wnsghgj1234@naver.com>
2021-10-14 12:23:49 +0900

    Implemented DoorBreakerCustom

--- ExtraEnemyCustomization/Customizations/Abilities/DoorBreakerCustom.cs
+++ ExtraEnemyCustomization/Customizations/Abilities/DoorBreakerCustom.cs
@@ -1,4 +1,6 @@
-﻿using Enemies;
+﻿using EECustom.Customizations.Abilities.Inject;
+using EECustom.Utils;
+using Enemies;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,7 +15,7 @@ namespace EECustom.Customizations.Abilities
         public float MinDelay { get; set; } = 0.5f;
         public float MaxDelay { get; set; } = 0.75f;
 
-        private float _GlobalTimer = 0.0f;
+        internal float GlobalTimer = 0.0f;
         
 
         public override string GetProcessName()
@@ -23,14 +25,28 @@ namespace EECustom.Customizations.Abilities
 
         public override void OnConfigLoaded()
         {
-            
+            EB_InCombat_MoveToNextNode_DestroyDoor.s_globalRetryTimer = float.MaxValue;
+            Inject_EB_DestroyDoor.ShouldOverride = true;
         }
 
         public void OnSpawned(EnemyAgent agent)
         {
-            //agent.AI.m_behaviour.m_currentStateName
-            throw new NotImplementedException();
-            //weakdoor.m_sync.AttemptDoorInteraction(eDoorInteractionType.DoDamage, 1.0f, 0f, sourcePos, null);
+            var setting = EnemyProperty<DoorBreakerSetting>.RegisterOrGet(agent);
+            setting.Config = this;
+            setting.Damage = Damage;
+            setting.UseGlobalTimer = UseGlobalTimer;
+            setting.MinDelay = MinDelay;
+            setting.MaxDelay = MaxDelay;
         }
+    }
+
+    public class DoorBreakerSetting
+    {
+        public DoorBreakerCustom Config;
+        public bool UseGlobalTimer = false;
+        public float Damage = 1.0f;
+        public float MinDelay = 0.5f;
+        public float MaxDelay = 0.75f;
+        public float Timer = 0.0f;
     }
 }
