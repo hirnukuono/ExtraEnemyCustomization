34e2e9a939591da61bc3237cb88f2af6ff4553a8
Chase Tuggle <chasetug@gmail.com>
2021-10-16 19:45:19 -0500

    Add effectvolume sphere

--- ExtraEnemyCustomization/Customizations/Abilities/FogSphereCustom.cs
+++ ExtraEnemyCustomization/Customizations/Abilities/FogSphereCustom.cs
@@ -1,9 +1,11 @@
-﻿using Enemies;
+﻿using System.Collections.Generic;
+using EECustom.Extensions;
+using Enemies;
 using UnityEngine;
 
 namespace EECustom.Customizations.Abilities
 {
-    public class FogSphereCustom : EnemyCustomBase, IEnemySpawnedEvent
+    public class FogSphereCustom : EnemyCustomBase, IEnemySpawnedEvent, IEnemyPrefabBuiltEvent
     {
         public Color ColorMin { get; set; } = Color.white;
         public Color ColorMax { get; set; } = Color.clear;
@@ -16,7 +18,14 @@ namespace EECustom.Customizations.Abilities
         public float DensityAmountMin { get; set; } = 0.0f;
         public float DensityAmountMax { get; set; } = 5.0f;
         public float Duration { get; set; } = 30.0f;
-
+        public bool EffectEnabled { get; set; } = false;
+        public eEffectVolumeContents EffectContents { get; set; } = eEffectVolumeContents.Infection;
+        public eEffectVolumeModification EffectModification { get; set; } = eEffectVolumeModification.Inflict;
+        public float EffectScale { get; set; } = 1f;
+        
+        private readonly List<(EAB_FogSphere, GameObject)> _ChangedList = new();
+        private readonly List<EV_Sphere> _EVSpheres = new();
+        
         public override string GetProcessName()
         {
             return "FogSphere";
@@ -24,21 +33,53 @@ namespace EECustom.Customizations.Abilities
 
         public void OnSpawned(EnemyAgent agent)
         {
-            var eabFog = agent.GetComponentInChildren<EAB_FogSphere>(true);
-            var fog = eabFog.m_fogSpherePrefab.GetComponent<FogSphereHandler>();
-            if (eabFog != null)
+            if (EffectEnabled)
             {
-                fog.m_colorMin = ColorMin;
-                fog.m_colorMax = ColorMax;
-                fog.m_intensityMin = IntensityMin;
-                fog.m_intensityMax = IntensityMax;
-                fog.m_rangeMin = RangeMin;
-                fog.m_rangeMax = RangeMax;
-                fog.m_densityMin = DensityMin;
-                fog.m_densityMax = DensityMax;
-                fog.m_densityAmountMin = DensityAmountMin;
-                fog.m_densityAmountMax = DensityAmountMax;
-                fog.m_totalLength = Duration;
+                var EVSphere = new EV_Sphere()
+                {
+                    position = agent.Position,
+                    minRadius = RangeMin,
+                    maxRadius = RangeMax,
+                    modificationScale = EffectScale,
+                    invert = EffectModification == eEffectVolumeModification.Shield,
+                    contents = EffectContents,
+                    modification = EffectModification
+                };
+                if (!_EVSpheres.Contains(EVSphere))
+                {
+                    EffectVolumeManager.RegisterVolume(EVSphere);
+                    _EVSpheres.Add(EVSphere);
+                }
+                else
+                {
+                    EffectVolumeManager.UnregisterVolume(EVSphere);
+                }
+                
+            }
+        }
+
+        public void OnPrefabBuilt(EnemyAgent agent)
+        {
+            var eabFog = agent.GetComponentInChildren<EAB_FogSphere>(true);
+            var fogPrefab = eabFog.m_fogSpherePrefab;
+            _ChangedList.Add((eabFog, fogPrefab));
+
+            var newFogPrefab = fogPrefab.Instantiate(agent.gameObject.transform, "newFogPrefab");
+            var fogHandler = eabFog.m_fogSpherePrefab.GetComponent<FogSphereHandler>();
+            
+            if (fogHandler != null)
+            {
+                fogHandler.m_colorMin = ColorMin;
+                fogHandler.m_colorMax = ColorMax;
+                fogHandler.m_intensityMin = IntensityMin;
+                fogHandler.m_intensityMax = IntensityMax;
+                fogHandler.m_rangeMin = RangeMin;
+                fogHandler.m_rangeMax = RangeMax;
+                fogHandler.m_densityMin = DensityMin;
+                fogHandler.m_densityMax = DensityMax;
+                fogHandler.m_densityAmountMin = DensityAmountMin;
+                fogHandler.m_densityAmountMax = DensityAmountMax;
+                fogHandler.m_totalLength = Duration;
             }
         }
     }
