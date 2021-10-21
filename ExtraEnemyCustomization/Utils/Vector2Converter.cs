52c63a7e975d49a823fa371fbc68e6af47c5977b
Flowaria <wnsghgj1234@naver.com>
2021-10-21 12:00:02 +0900

    Added Vector Converters

---
+++ ExtraEnemyCustomization/Utils/Vector2Converter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using UnityEngine;
+
+namespace EECustom.Utils
+{
+    //MINOR: Implement this
+    public class Vector2Converter : JsonConverter<Vector2>
+    {
+        public override Vector2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var vector = new Vector2();
+
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.StartObject:
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonTokenType.EndObject)
+                            return vector;
+
+                        if (reader.TokenType != JsonTokenType.PropertyName)
+                            throw new JsonException("Expected PropertyName token");
+
+                        var propName = reader.GetString();
+                        reader.Read();
+
+                        switch (propName.ToLower())
+                        {
+                            case "x":
+                                vector.x = reader.GetSingle();
+                                break;
+
+                            case "y":
+                                vector.y = reader.GetSingle();
+                                break;
+                        }
+                    }
+                    throw new JsonException("Expected EndObject token");
+
+                case JsonTokenType.String:
+                    var strValue = reader.GetString().Trim();
+                    if (TryParseVector2(strValue, out vector))
+                    {
+                        return vector;
+                    }
+                    throw new JsonException($"Vector2 format is not right: {strValue}");
+
+                default:
+                    throw new JsonException($"Vector2Json type: {reader.TokenType} is not implemented!");
+            }
+        }
+
+        private bool TryParseVector2(string input, out Vector2 vector)
+        {
+            if (!input.Contains(",", StringComparison.Ordinal))
+            {
+                vector = Vector2.zero;
+                return false;
+            }
+
+            var split = input.Split(",");
+            if (split.Length != 2)
+            {
+                vector = Vector2.zero;
+                return false;
+            }
+
+            if (!float.TryParse(split[0].Trim(), out var x))
+            {
+                vector = Vector2.zero;
+                return false;
+            }
+
+            if (!float.TryParse(split[1].Trim(), out var y))
+            {
+                vector = Vector2.zero;
+                return false;
+            }
+
+            vector = new Vector2(x, y);
+            return true;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Vector2 value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExtraEnemyCustomization/Utils/Vector3Converter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using UnityEngine;
+
+namespace EECustom.Utils
+{
+    public class Vector3Converter : JsonConverter<Vector3>
+    {
+        public override bool HandleNull => false;
+
+        public override Vector3 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var vector = new Vector3();
+
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.StartObject:
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonTokenType.EndObject)
+                            return vector;
+
+                        if (reader.TokenType != JsonTokenType.PropertyName)
+                            throw new JsonException("Expected PropertyName token");
+
+                        var propName = reader.GetString();
+                        reader.Read();
+
+                        switch (propName.ToLower())
+                        {
+                            case "x":
+                                vector.x = reader.GetSingle();
+                                break;
+
+                            case "y":
+                                vector.y = reader.GetSingle();
+                                break;
+
+                            case "z":
+                                vector.z = reader.GetSingle();
+                                break;
+                        }
+                    }
+                    throw new JsonException("Expected EndObject token");
+
+                case JsonTokenType.String:
+                    var strValue = reader.GetString().Trim();
+                    if (TryParseVector3(strValue, out vector))
+                    {
+                        return vector;
+                    }
+                    throw new JsonException($"Vector3 format is not right: {strValue}");
+
+                default:
+                    throw new JsonException($"Vector3Json type: {reader.TokenType} is not implemented!");
+            }
+        }
+
+        private bool TryParseVector3(string input, out Vector3 vector)
+        {
+            if (!input.Contains(",", StringComparison.Ordinal))
+            {
+                vector = Vector3.zero;
+                return false;
+            }
+
+            var split = input.Split(",");
+            if (split.Length != 3)
+            {
+                vector = Vector3.zero;
+                return false;
+            }
+
+            if (!float.TryParse(split[0].Trim(), out var x))
+            {
+                vector = Vector3.zero;
+                return false;
+            }
+
+            if (!float.TryParse(split[1].Trim(), out var y))
+            {
+                vector = Vector3.zero;
+                return false;
+            }
+
+            if (!float.TryParse(split[2].Trim(), out var z))
+            {
+                vector = Vector3.zero;
+                return false;
+            }
+
+            vector = new Vector3(x, y, z);
+            return true;
+        }
+
+        public override void Write(Utf8JsonWriter writer, Vector3 value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
