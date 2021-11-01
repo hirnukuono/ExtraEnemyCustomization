051bf390bd7f18949e06443b4fc2658ed46b5b7b
Flowaria <wnsghgj1234@naver.com>
2021-11-01 19:50:40 +0900

    Obsolete IEnemyDespawnedEvent

--- ExtraEnemyCustomization/Customizations/IEnemyDespawnedEvent.cs
+++ ExtraEnemyCustomization/Customizations/IEnemyDespawnedEvent.cs
@@ -1,7 +1,9 @@
 ﻿using Enemies;
+using System;
 
 namespace EECustom.Customizations
 {
+    [Obsolete("Using Despawned Event will not work since ID will be unregiered on dead!", false)]
     public interface IEnemyDespawnedEvent : IEnemyEvent
     {
         void OnDespawned(EnemyAgent agent);
