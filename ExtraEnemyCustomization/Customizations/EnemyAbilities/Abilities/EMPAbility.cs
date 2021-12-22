4d9a81d4882ccd1749c70ce11d00ed67ff245a30
Dak <vectorskeleton@gmail.com>
2021-12-22 10:14:50 -0800

    Add new settings

--- ExtraEnemyCustomization/Customizations/EnemyAbilities/Abilities/EMPAbility.cs
+++ ExtraEnemyCustomization/Customizations/EnemyAbilities/Abilities/EMPAbility.cs
@@ -5,14 +5,18 @@ namespace EECustom.Customizations.EnemyAbilities.Abilities
 {
     public class EMPAbility : AbilityBase<EMPBehaviour>
     {
+        public uint ChargeUpSoundId { get; set; } = 0u;
+        public uint ActivateSoundId { get; set; } = 0u;
+        public int ChargeUpDuration { get; set; } = 5;
+        public float  EffectDuration { get; set; } = 30;
+        public int EffectRange { get; set; } = 20;
+        public bool InvincibleWhileCharging { get; set; } = true;
         public Color FogColor { get; set; } = new Color(0.525f, 0.956f, 0.886f, 1.0f);
         public float FogIntensity { get; set; } = 1.0f;
         public Color BuildupColor { get; set; } = new Color(0.525f, 0.956f, 0.886f, 1.0f) * 2.0f;
         public Color ScreamColor { get; set; } = new Color(0.525f, 0.956f, 0.886f, 1.0f) * 20.0f;
     }
 
-    //MAJOR: Please Implement this Dak
-    [Obsolete("PLEASE IMPLEMENT THIS DAK", false)]
     public class EMPBehaviour : AbilityBehaviour<EMPAbility>
     {
         private float _stateTimer;
