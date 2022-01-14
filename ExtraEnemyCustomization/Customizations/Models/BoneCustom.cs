ad0aaa0945f2f20bc86605920fc39a30af9475cf
Flowaria <wnsghgj1234@naver.com>
2022-01-15 06:43:19 +0900

    Fixed Bug: BoneCustom Scale/Offset now work as intended

--- ExtraEnemyCustomization/Customizations/Models/BoneCustom.cs
+++ ExtraEnemyCustomization/Customizations/Models/BoneCustom.cs
@@ -1,4 +1,5 @@
-﻿using Enemies;
+﻿using EECustom.Events;
+using Enemies;
 using System;
 using UnityEngine;
 
@@ -26,9 +27,33 @@ namespace EECustom.Customizations.Models
             try
             {
                 var transform = agent.Anim.GetBoneTransform(boneTransform.Bone);
-                transform.localPosition += boneTransform.Offset;
-                transform.localScale = boneTransform.Scale;
-                transform.localRotation.SetEulerAngles(transform.localEulerAngles + boneTransform.RotationOffset);
+                if (boneTransform.Offset != Vector3.zero || boneTransform.Offset != Vector3.zero)
+                {
+                    MonoBehaviourEventHandler.AttatchToObject(transform.gameObject, onLateUpdate: (GameObject g) =>
+                    {
+                        if (agent.Locomotion.m_animator.enabled)
+                        {
+                            transform.localPosition += boneTransform.Offset;
+                            transform.localEulerAngles += boneTransform.RotationOffset;
+                        }
+                    });
+                }
+                
+                if (boneTransform.UsingRelativeScale)
+                {
+                    var newScale = new Vector3()
+                    {
+                        x = transform.localScale.x * boneTransform.Scale.x,
+                        y = transform.localScale.y * boneTransform.Scale.y,
+                        z = transform.localScale.z * boneTransform.Scale.z
+                    };
+                    transform.localScale = newScale;
+                }
+                else
+                {
+                    transform.localScale = boneTransform.Scale;
+                }
+                
                 LogVerbose($"Applied Bone Setting: {transform.name}, offset: {boneTransform.Offset}, scale: {boneTransform.Scale} rotationOffset: {boneTransform.RotationOffset}");
             }
             catch (Exception e)
@@ -41,8 +66,9 @@ namespace EECustom.Customizations.Models
     public class BoneTransform
     {
         public HumanBodyBones Bone { get; set; } = HumanBodyBones.Head;
-        public Vector3 Offset { get; set; } = Vector3.zero;
+        public bool UsingRelativeScale { get; set; } = false;
         public Vector3 Scale { get; set; } = Vector3.zero;
+        public Vector3 Offset { get; set; } = Vector3.zero;
         public Vector3 RotationOffset { get; set; } = Vector3.zero;
     }
 }
