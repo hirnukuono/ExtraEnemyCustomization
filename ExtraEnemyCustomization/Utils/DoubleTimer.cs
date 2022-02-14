d5ab3b1770682606045d0b997f0ac51d876bd26c
Flowaria <wnsghgj1234@naver.com>
2022-02-14 09:48:52 +0900

    Added Timer Util

---
+++ ExtraEnemyCustomization/Utils/DoubleTimer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EECustom.Utils
+{
+    public struct DoubleTimer
+    {
+        public double Timer { get; private set; }
+        public double Duration { get; private set; }
+
+        public double Progress => Math.Clamp(0.0, 1.0, ProgressUnclamped);
+        public double ProgressUnclamped
+        {
+            get
+            {
+                if (Duration != 0.0)
+                {
+                    return (Timer / Duration);
+                }
+                return 1.0;
+            }
+        }
+
+        public float FloatProgress => Mathf.Clamp01(FloatProgressUnclamped);
+        public float FloatProgressUnclamped => (float)ProgressUnclamped;
+
+        public DoubleTimer(double duration)
+        {
+            Timer = 0.0;
+            Duration = duration;
+        }
+
+        public void Reset(float newDuration = -1.0f) => Reset((double)newDuration);
+        public void Reset(double newDuration = -1.0)
+        {
+            Timer = 0.0;
+            if (newDuration >= 0.0)
+            {
+                Duration = newDuration;
+            }
+        }
+
+        public void Tick()
+        {
+            Timer += Clock.Delta;
+        }
+    }
+}
