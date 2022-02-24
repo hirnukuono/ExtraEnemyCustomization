aec67f7b29c27caf45562490773948c315317fb7
Flowaria <wnsghgj1234@naver.com>
2022-02-24 09:34:48 +0900

    Marked SpawnCostCustomConfig as Obsolete

--- ExtraEnemyCustomization/Configs/Customizations/SpawnCostCustomConfig.cs
+++ ExtraEnemyCustomization/Configs/Customizations/SpawnCostCustomConfig.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace EECustom.Configs.Customizations
 {
+    [Obsolete("SpawnCost Config will be merged to Property in 2.x version!")]
     public sealed class SpawnCostCustomConfig : CustomizationConfig
     {
         public SpawnCostCustom[] SpawnCostCustom { get; set; } = Array.Empty<SpawnCostCustom>();
