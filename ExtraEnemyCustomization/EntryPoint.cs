5ae6ee505425bee680ffb9abe49918c16d752b8c
Flowaria <wnsghgj1234@naver.com>
2022-03-18 00:34:20 +0900

    Version Bump (1.4.1)

--- ExtraEnemyCustomization/EntryPoint.cs
+++ ExtraEnemyCustomization/EntryPoint.cs
@@ -24,7 +24,7 @@ namespace EECustom
     //TODO: - Patrolling Hibernation : Too many works to do with this one, this is one of the long term goal
     //TODO: Refactor the CustomBase to support Phase Setting
 
-    [BepInPlugin("GTFO.EECustomization", "EECustom", "1.4.0")]
+    [BepInPlugin("GTFO.EECustomization", "EECustom", "1.4.1")]
     [BepInProcess("GTFO.exe")]
     [BepInDependency(MTFOUtil.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
