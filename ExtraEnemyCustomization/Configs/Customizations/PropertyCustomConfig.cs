2f860c516b75c10921b5d11d266abb3ef27e2604
Flowaria <wnsghgj1234@naver.com>
2022-03-17 20:33:55 +0900

    Added DistantRoarCustom

--- ExtraEnemyCustomization/Configs/Customizations/PropertyCustomConfig.cs
+++ ExtraEnemyCustomization/Configs/Customizations/PropertyCustomConfig.cs
@@ -7,6 +7,7 @@ namespace EECustom.Configs.Customizations
     {
         public SpawnCostCustom[] SpawnCostCustom { get; set; } = Array.Empty<SpawnCostCustom>();
         public EventsCustom[] EventsCustom { get; set; } = Array.Empty<EventsCustom>();
+        public DistantRoarCustom[] DistantRoarCustom { get; set; } = Array.Empty<DistantRoarCustom>();
 
         public override string FileName => "Property";
         public override CustomizationConfigType Type => CustomizationConfigType.Property;
---
+++ ExtraEnemyCustomization/Customizations/Properties/DistantRoarCustom.cs
@@ -0,0 +1,44 @@
+﻿using Enemies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EECustom.Customizations.Properties
+{
+    public sealed class DistantRoarCustom : EnemyCustomBase, IEnemySpawnedEvent
+    {
+        public uint SoundID { get; set; } = 0u;
+        public float Interval { get; set; } = 0.0f;
+
+        private float _timer = 0.0f;
+        private CellSoundPlayer _soundPlayer;
+
+        public override string GetProcessName()
+        {
+            return "DistantRoar";
+        }
+
+        public override void OnConfigLoaded()
+        {
+            _soundPlayer = new();
+        }
+
+        public override void OnConfigUnloaded()
+        {
+            _soundPlayer?.Recycle();
+        }
+
+        public void OnSpawned(EnemyAgent agent)
+        {
+            if (_timer <= Clock.Time)
+            {
+                if (_soundPlayer != null)
+                {
+                    _soundPlayer.UpdatePosition(agent.Position);
+                    _soundPlayer.Post(SoundID, isGlobal: true);
+                }
+                _timer = Clock.Time + Interval;
+            }
+        }
+    }
+}
