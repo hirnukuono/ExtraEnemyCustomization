df8d4e592239c4316ba2acce5466248ac2b869ad
Flowaria <wnsghgj1234@naver.com>
2022-03-24 10:30:37 +0900

    Added BoolBase for Unchanged Condition booleans

---
+++ ExtraEnemyCustomization/Utils/Json/Elements/BoolBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace EEC.Utils.Json.Elements
+{
+    [JsonConverter(typeof(BoolBaseConverter))]
+    public struct BoolBase
+    {
+        public static readonly BoolBase False = new(BoolMode.False);
+        public static readonly BoolBase True = new(BoolMode.True);
+        public static readonly BoolBase Unchanged = new(BoolMode.Unchanged);
+
+        public BoolMode Mode;
+
+        public BoolBase(bool mode)
+        {
+            Mode = mode ? BoolMode.True : BoolMode.False;
+        }
+
+        public BoolBase(BoolMode mode)
+        {
+            Mode = mode;
+        }
+
+        public bool GetValue(bool originalValue)
+        {
+            switch (Mode)
+            {
+                case BoolMode.Unchanged:
+                    return originalValue;
+                case BoolMode.True:
+                    return true;
+                case BoolMode.False:
+                    return false;
+                default:
+                    Logger.Error($"BoolBase.GetValue; Got Unknown Mode: {Mode}!\n{Environment.StackTrace}");
+                    return originalValue;
+            }
+        }
+    }
+
+    public enum BoolMode
+    {
+        False,
+        True,
+        Unchanged
+    }
+}
---
+++ ExtraEnemyCustomization/Utils/Json/Elements/BoolBaseConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EEC.Utils.Json.Elements
+{
+    public sealed class BoolBaseConverter : JsonConverter<BoolBase>
+    {
+        public override bool HandleNull => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ValueBase);
+        }
+
+        public override BoolBase Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    var strValue = reader.GetString().Trim();
+                    if (strValue.EqualsAnyIgnoreCase("Unchanged", "Ignore", "Keep", "Original", "KeepOriginal"))
+                    {
+                        return BoolBase.Unchanged;
+                    }
+                    else if (bool.TryParse(strValue, out var parsedValue))
+                    {
+                        return new BoolBase(parsedValue);
+                    }
+                    throw new JsonException($"Cannot parse BoolBase string: {strValue}! Are you sure it's in right format?");
+
+                case JsonTokenType.True:
+                    return BoolBase.True;
+
+                case JsonTokenType.False:
+                    return BoolBase.False;
+
+                default:
+                    throw new JsonException($"BoolBaseJson type: {reader.TokenType} is not implemented!");
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, BoolBase value, JsonSerializerOptions options)
+        {
+            switch (value.Mode)
+            {
+                case BoolMode.True:
+                    writer.WriteBooleanValue(true);
+                    break;
+
+                case BoolMode.False:
+                    writer.WriteBooleanValue(false);
+                    break;
+
+                case BoolMode.Unchanged:
+                    writer.WriteStringValue("Unchanged");
+                    break;
+            }
+        }
+    }
+}
