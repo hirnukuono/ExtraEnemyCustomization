5514526750625418ceceeaaedac1a31eed037b58
Dinorush <62536724+Dinorush@users.noreply.github.com>
2026-09-24 13:38:28 -0400

    Fix a few leaks, add IgnoreDespawn to DeathAbility (#24)

---
+++ ExtraEnemyCustomization/Managers/Properties/EnemyDeathManager.cs
@@ -0,0 +1,39 @@
+﻿using EEC.Events;
+using Enemies;
+
+namespace EEC.Managers.Properties
+{
+    [CallConstructorOnLoad]
+    public static class EnemyDeathManager
+    {
+        private static readonly Dictionary<ushort, bool> _killedLookup = new(30);
+
+        static EnemyDeathManager()
+        {
+            EnemyEvents.Killed += Killed;
+            EnemyEvents.Despawn += Despawn;
+            LevelEvents.LevelCleanup += OnLevelCleanup;
+        }
+
+        private static void Killed(EnemyAgent agent)
+        {
+            _killedLookup[agent.GlobalID] = true;
+        }
+
+        private static void Despawn(EnemyAgent agent)
+        {
+            _killedLookup.Remove(agent.GlobalID);
+        }
+
+        private static void OnLevelCleanup()
+        {
+            _killedLookup.Clear();
+        }
+
+        public static bool WasKilled(EnemyAgent enemy) => WasKilled(enemy.GlobalID);
+        public static bool WasKilled(ushort id)
+        {
+            return _killedLookup.GetValueOrDefault(id);
+        }
+    }
+}
